547366bc2e152547b03dff07ba1375504a7dbc09
Harshit Jain <jnharshit1998@gmail.com>
2026-06-24 12:31:54 +0000

    removed style creator, item visualizer and search

--- ec_production/hooks.py
+++ ec_production/hooks.py
@@ -48,18 +48,18 @@ app_license = "mit"
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
 
-doctype_js = {
-    "Sales Order": "public/js/advanced_item_search.js",
-    "Quotation": "public/js/advanced_item_search.js",
-    "Delivery Note": "public/js/advanced_item_search.js",
-    "Sales Invoice": "public/js/advanced_item_search.js",
-    "Purchase Order": "public/js/advanced_item_search.js",
-    "Purchase Receipt": "public/js/advanced_item_search.js",
-}
+# doctype_js = {
+#     "Sales Order": "public/js/advanced_item_search.js",
+#     "Quotation": "public/js/advanced_item_search.js",
+#     "Delivery Note": "public/js/advanced_item_search.js",
+#     "Sales Invoice": "public/js/advanced_item_search.js",
+#     "Purchase Order": "public/js/advanced_item_search.js",
+#     "Purchase Receipt": "public/js/advanced_item_search.js",
+# }
 
-doctype_list_js = {
-    "Item": "public/js/item_list.js",
-}
+# doctype_list_js = {
+#     "Item": "public/js/item_list.js",
+# }
 
 # Svg Icons
 # ------------------
